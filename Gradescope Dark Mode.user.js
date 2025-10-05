c3a3c5b0afe999da9bbba2ea40fd22d9078bd230
Erik Demaine <edemaine@mit.edu>
2025-10-05 17:21:34 -0400

    Fix Gradescope page thumbnails

--- Gradescope Dark Mode.user.js
+++ Gradescope Dark Mode.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Gradescope Dark Mode
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Invert color of PDF viewer. Adjust brightness via -/+ keys.
 // @author       Erik Demaine
 // @match        https://www.gradescope.com/*
@@ -17,7 +17,7 @@
     style.type = 'text/css';
     document.head.appendChild(style);
 
-    const update = () => style.innerText = `.pv--viewer img { filter: invert(1) hue-rotate(180deg) brightness(${brightness}) }`;
+    const update = () => style.innerText = `.pv--viewer img, .pageThumbnail img { filter: invert(1) hue-rotate(180deg) brightness(${brightness}) }`;
     update();
 
     window.addEventListener('keydown', (e) => {
